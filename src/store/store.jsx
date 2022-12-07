a9d9f276875f33a9e026b239d386625090ae37d7
Saurajit Bhuyan <saurajitb@gmail.com>
2022-12-08 00:36:43 +0530

    Add thunk

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,11 +1,12 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import thunk from 'redux-thunk';
 import { rootReducer } from '../redux/reducers'
 import rootSaga from '../saga'
 
 export default function configureStore() {
   const sagaMiddleware = createSagaMiddleware();
-  const middleWareEnhancer = applyMiddleware(sagaMiddleware);
+  const middleWareEnhancer = applyMiddleware(sagaMiddleware, thunk);
   // eslint-disable-next-line no-underscore-dangle
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
